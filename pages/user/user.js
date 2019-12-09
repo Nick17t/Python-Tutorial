99ad48bbafc1f1e3440e8ccbc29e56771ec22acd
tridoron <oncwnuPZ9JW2hDQ5RpsUNPVQxXHI@git.weixin.qq.com>
2019-12-09 17:06:02 +0800

    user页面追加登录功能

--- pages/user/user.js
+++ pages/user/user.js
@@ -6,20 +6,30 @@ Page({
     userInfo: {}
   },
   //事件处理函数
-  bindViewTap: function () {
+  bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
+
+  onLoad: function() {
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function (userInfo) {
+    app.getUserInfo(function(userInfo) {
       //更新数据
       that.setData({
         userInfo: userInfo
       })
+    })
+  },
+  
+  getMyInfo: function(e) {
+    let info = e.detail.userInfo;
+    this.setData({
+      isLogin: true,
+      src: info.avatarUrl,
+      nickName: info.nickName
     })
   }
 })
