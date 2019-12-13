a6857b7ef6d5dd329e581f382bd7a0fd58d10fc4
tridoron <oncwnuPZ9JW2hDQ5RpsUNPVQxXHI@git.weixin.qq.com>
2019-12-13 15:38:56 +0800

    完善字数统计功能

--- pages/issue/issue.js
+++ pages/issue/issue.js
@@ -6,7 +6,7 @@ Page({
    */
   data: {
     maxNumber: 500, //可输入最大字数
-    number: 0, //已输入字数
+    currentWordNumber: 0, //已输入字数
 
     selectorItems: ['QQ', '微信', '手机', '邮箱', '不留联系方式'],
     pickerIndex: 0
@@ -68,18 +68,18 @@ Page({
 
   },
 
-  inputText: function(e) { //监听输入，实时改变已输入字数
+  getValueLength: function(e) { //监听输入，实时改变已输入字数
     let value = e.detail.value; //获取textarea的内容，
-    let len = value.length; //获取textarea的内容长度
+    let len = parseInt(value.length); //获取textarea的内容长度
     this.setData({
-      'number': len
+      currentWordNumber: len
     })
   },
 
   selectorChange: function(e) {
     var that = this
     let index = e.currentTarget.dataset.value
-    that.setData({//点击后更新数据
+    that.setData({ //点击后更新数据
       [index]: e.detail.value
     });
   }
