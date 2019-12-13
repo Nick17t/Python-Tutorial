6276d68abb36bec68f48635b322c6cb99a2654c3
tridoron <oncwnuPZ9JW2hDQ5RpsUNPVQxXHI@git.weixin.qq.com>
2019-12-13 19:26:48 +0800

    完善主页

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,11 +6,52 @@ var WxSearch = require('../../wxSearchView/wxSearchView.js');
 const app = getApp()
 
 Page({
-  // data: {
-  //   userInfo: {},
-  //   hasUserInfo: false,
-  //   canIUse: wx.canIUse('button.open-type.getUserInfo')
-  // },
+  data: {
+    list: [{
+        "useravatar": "../../image/testinfo/testavatar/1.jpg",
+        "username": "zcz",
+        "issueType": "丢失物品",
+        "photos": "../../image/testinfo/testphoto/lost-1.jpg",
+        "title": "丢失钥匙一串",
+        "text": "在一食堂附近丢失一串如图所示的钥匙，请捡到的同学联系我",
+        "position": "一食堂",
+        "issuedate": "2019-12-03"
+      },
+      {
+        "useravatar": "../../image/testinfo/testavatar/2.jpg",
+        "username": "zs",
+        "issueType": "捡到物品",
+        "photos": "../../image/testinfo/testphoto/found-2.jpg",
+        "title": "捡到校园卡",
+        "text": "在信息楼313捡到学号171003530112的校园卡，放在讲台上",
+        "position": "信息楼313",
+        "issuedate": "2019-12-02"
+      },
+      {
+        "useravatar": "../../image/testinfo/testavatar/3.jpg",
+        "username": "zzc",
+        "issueType": "捡到物品",
+        "photos": "../../image/testinfo/testphoto/found-1.jpg",
+        "title": "捡到U盘",
+        "text": "在25宿1楼捡到1个U盘，放在门口桌子上了",
+        "position": "25-26宿",
+        "issuedate": "2019-12-01"
+      },
+      {
+        "useravatar": "../../image/testinfo/testavatar/4.jpg",
+        "username": "sz",
+        "issueType": "丢失物品",
+        "photos": "../../image/testinfo/testphoto/lost-2.jpg",
+        "title": "丢了一本书",
+        "text": "丢了一本《C++ Primier》，可能是在A教",
+        "position": "A教或其他地方",
+        "issuedate": "2019-11-30"
+      }
+    ],
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
   // //事件处理函数
   // bindViewTap: function() {
   //   wx.navigateTo({
@@ -44,6 +85,17 @@ Page({
     //     }
     //   })
     // }
+
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo) {
+      //更新数据
+      that.setData({
+        userInfo: userInfo
+      })
+    });
+
     var that = this;
     WxSearch.init(
       that, // 本页面一个引用
@@ -53,13 +105,13 @@ Page({
       that.myGobackFunction //提供一个返回回调函数
     );
   },
-  wxSearchInput: WxSearch.wxSearchInput,  // 输入变化时的操作
-  wxSearchKeyTap: WxSearch.wxSearchKeyTap,  // 点击提示或者关键字、历史记录时的操作
+  wxSearchInput: WxSearch.wxSearchInput, // 输入变化时的操作
+  wxSearchKeyTap: WxSearch.wxSearchKeyTap, // 点击提示或者关键字、历史记录时的操作
   wxSearchDeleteAll: WxSearch.wxSearchDeleteAll, // 删除所有的历史记录
-  wxSearchConfirm: WxSearch.wxSearchConfirm,  // 搜索函数
-  wxSearchClear: WxSearch.wxSearchClear,  // 清空函数
-  
-  mySearchFunction: function (value) {
+  wxSearchConfirm: WxSearch.wxSearchConfirm, // 搜索函数
+  wxSearchClear: WxSearch.wxSearchClear, // 清空函数
+
+  mySearchFunction: function(value) {
     // do your job here
     // 示例：跳转
     wx.redirectTo({
@@ -68,7 +120,7 @@ Page({
   },
 
   // 5 返回回调函数
-  myGobackFunction: function () {
+  myGobackFunction: function() {
     // do your job here
     // 示例：返回
     wx.redirectTo({
