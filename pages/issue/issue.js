d5f4e99542e39032c0a6e2312c9bf638afaf24db
tridoron <oncwnuPZ9JW2hDQ5RpsUNPVQxXHI@git.weixin.qq.com>
2019-12-15 16:55:57 +0800

    发布页面表单提交功能实现

--- pages/issue/issue.js
+++ pages/issue/issue.js
@@ -82,5 +82,41 @@ Page({
     that.setData({ //点击后更新数据
       [index]: e.detail.value
     });
+  },
+
+  // formSubmit: function (e) {
+  //   console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  //   this.setData({
+  //     allValue: e.detail.value
+  //   })
+  // },
+
+  formSubmit: function (e) {
+    var _this = this;
+    wx.request({
+      url: 'http://localhost:8080/thelostfound_war_exploded/issue',
+      method: 'POST',
+      data:{
+        issueType:e.detail.value.issueType,
+        title:e.detail.value.title,
+        position:e.detail.value.position,
+        text:e.detail.value.text
+      },
+      header: {
+        //'content-type': 'application/json' // 默认值
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        console.log(res.data);
+        _this.setData({
+          title: "",
+          position: "",
+          text: ""
+        });
+      },
+      fail: function (res) {
+        console.log("上传失败");
+      }
+    })
   }
 })
